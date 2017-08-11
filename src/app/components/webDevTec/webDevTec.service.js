b86e8500a01dc0664e3a56af2512b4599aed7711
Gaston Trujillo <gaston.trujillo.java@gmail.com>
2017-08-11 09:10:25 -0500

    Purchase: Redo API url for production

--- src/app/components/webDevTec/webDevTec.service.js
+++ src/app/components/webDevTec/webDevTec.service.js
@@ -7,25 +7,25 @@ export class WebDevTecService {
   }
 
   getProducts() {
-    return this.$http.get(this.localUrlBase + 'products');
+    return this.$http.get(this.urlBase + 'products');
   }
 
   signup(user) {
-    return this.$http.post(this.localUrlBase + 'users', { user: user });
+    return this.$http.post(this.urlBase + 'users', { user: user });
   }
 
   login(user) {
-    return this.$http.post(this.localUrlBase + 'users/sign_in?email=' + user.email + '&password=' + user.password);
+    return this.$http.post(this.urlBase + 'users/sign_in?email=' + user.email + '&password=' + user.password);
 
   }
 
   logout() {
-    return this.$http.delete(this.localUrlBase + 'users/sign_out');
+    return this.$http.delete(this.urlBase + 'users/sign_out');
   }
 
   saveCart(user, cart) {
     return this.$http.post(
-      this.localUrlBase + 'users/' + user.id + '/carts', { cart: cart },
+      this.urlBase + 'users/' + user.id + '/carts', { cart: cart },
       {
         headers: {
           'X-API-EMAIL': user.email,
